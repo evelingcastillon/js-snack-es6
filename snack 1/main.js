7703b218e5319229455802f42d16349be99a8934
evelingcastillon <evelingcastillon0304@gmail.com>
2021-05-11 23:38:27 +0200

    create array

---
+++ snack 1/main.js
@@ -0,0 +1,15 @@
+/* Creare un array di oggetti, ogni oggetto descriverà
+una bici da corsa con le seguenti proprietà:
+nome
+peso */
+
+//Stampare a schermo la bici con peso minore utilizzando
+//destructuring e template literal
+const bici = [
+    {nome: 'bici da città', peso: 12},
+    {nome: 'mountain bike', peso: 14},
+    {nome: 'bici elettrica', peso: 30},
+    {nome: 'bici da corsa', peso: 10},
+    {nome: 'bicicletta da cicloturismo', peso: 14},
+    {nome: 'bici pieghevole', peso: 16},
+]
