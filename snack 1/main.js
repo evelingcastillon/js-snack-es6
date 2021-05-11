b73876f7ae803aec6bc635f0252bdef6a08d22fc
evelingcastillon <evelingcastillon0304@gmail.com>
2021-05-12 00:05:44 +0200

    add destructuring

--- snack 1/main.js
+++ snack 1/main.js
@@ -3,8 +3,6 @@ una bici da corsa con le seguenti proprietà:
 nome
 peso */
 
-//Stampare a schermo la bici con peso minore utilizzando
-//destructuring e template literal
 const bici = [
     {nome: 'bici da città', peso: 12},
     {nome: 'mountain bike', peso: 14},
@@ -12,4 +10,10 @@ const bici = [
     {nome: 'bici da corsa', peso: 10},
     {nome: 'bicicletta da cicloturismo', peso: 14},
     {nome: 'bici pieghevole', peso: 16},
-]
+];
+
+//Stampare a schermo la bici con peso minore utilizzando
+//destructuring e template literal
+const [primabici, , ,quartabici , ,] = bici;
+
+console.log(primabici, quartabici);
